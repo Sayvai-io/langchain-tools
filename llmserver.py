2c19a14cdba9c119d4fbbbc3deb812d72c926a04
Nandhini25S <nandhinivadalur@gmail.com>
2023-08-17 20:23:04 +0530

    Initialiazed LLMs

--- llmserver.py
+++ llmserver.py
@@ -6,7 +6,10 @@ from typing import List, Dict, Any
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 # from langchain.llms import OpenAI
-
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SimpleSequentialChain
 
 with open("OPEN_AI_KEY.txt", "r") as f:
     OPEN_AI_KEY = f.read()
@@ -29,15 +32,19 @@ class LlmServer:
         self.tools = None
         self.agent = None
     
-    def initialize(self, tools: List, agent: str) -> str:
+    def initialize_agent(self, tools: List, agent: str) -> str:
         """Initialize the server"""
         if self.tools is None:
             self.tools = load_tools(tools, llm=self.llm)
         self.agent = initialize_agent(self.tools, self.llm, agent, verbose=False)
         return "Initialized"
     
+    def initialize_llm(self):
+    
     def get_response(self, text: str) -> Dict[str, Any]:
         if self.agent is None:
             raise CustomError('Not Initialized error')
         response = self.agent.run(text)
         return response
+
+    
